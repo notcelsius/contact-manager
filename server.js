f5a71071d15933cbf0f3954f2625125f6b40b177
notcelsius <149901647+notcelsius@users.noreply.github.com>
2025-09-22 00:22:55 -0700

    Integrated CRUD operations with the DB

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const errorHandler = require("./middleware/errorhandler")
+const connectDb = require("./config/dbConnection")
 const dotenv = require("dotenv").config()
 const app = express()
 
+connectDb();
 const port = process.env.PORT || 5000
 
 app.use(express.json()) // parses data stream that we receive from client on the server side
